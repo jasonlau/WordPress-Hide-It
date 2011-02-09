47297eb0efd6cd810d2a10d3520eb1263f22ba1a
Jason Lau <jason_lau@live.com>
2011-02-09 11:03:07 -0600

    Version 1.0.0

---
+++ README.txt
@@ -0,0 +1,45 @@
+=== Hide It ===
+
+Contributors: Jason Lau
+Donate link: http://jasonlau.biz
+Tags: hide, show, visible, visibility, opacity, widget, content, post, page, style, hidden, display, none, toggle, appear, appearance, transparent, opaque
+Requires at least: 2.8.4 
+Stable tag: 1.0.0
+Tested up to: 3.0.4
+
+== Description ==
+
+This plugin allows you to toggle visibility of any objects from within a post or page using shorttags and CSS class name or id attribute selectors.
+
+== Installation ==
+
+*Use the Plugin installer which is located in the WordPress Dashboard menu under "Plugins > Add New". OR ...
+*Download hide-it.zip.
+*Unzip hide-it.zip. 
+*Upload the folder named 'hide-it' to the wp-content/plugins directory on your server
+*Go to the plugins manager in your Wordpress Dashboard and locate "Hide It" in the list of plugins (under 'H' for 'Hide It').
+*Click Activate.
+*Go to my website and make a generous monetary donation to show your love!  
+
+Once the plugin is activated, you will find "Hide It" in the Dashboard menu under "Tools".
+
+== Frequently Asked Questions ==
+
+= Do you accept donations? =
+Yes I do. Thank you in advance!
+= Are you single? =
+Why yes, I am!
+
+== Screenshots ==
+
+1. screenshot-1.png Is a picture of the "Hide It" Admin panel.
+   
+== Changelog ==
+
+= 1.0.0 =
+* Initial release. Prayers have been answered.
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Initial release.
---
+++ hide-it/plugin.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package Hide It
+ * @version 1.0.0 
+ * @author Jason Lau
+ * @link http://jasonlau.biz
+ * @copyright 2011
+ * @license GNU/GPL 3+
+ * @uses WordPress
+ 
+Plugin Name: Hide It
+Plugin URI: http://jasonlau.biz
+Description: This plugin allows you to toggle visibility of any objects from within a post or page using shorttags and CSS class name or id attribute selectors. Example: [hideit hide=".widget-1, #header-div" show=".widget-2"]. A .dot indicates this is a class name, a #hash mark indicates this is an id attribute, and a comma separates each object. Easy enough. You can get an object's id or class name by viewing the source code for your webpage.
+Author: Jason Lau
+Version: 1.0.0
+Author URI: http://jasonlau.biz
+*/
+
+if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+    exit('Please don\'t access this file directly.');
+}
+
+function hideit_manager(){
+?>
+<div class="wrap">
+<div id="icon-tools" class="icon32"><br /></div>
+<h2>Hide It</h2>
+<hr />
+<p><strong>Hide It</strong> allows you to toggle visibility of any objects from within a post or page using shorttags and CSS class name or id attribute selectors.</p>
+<p><strong>Hide It</strong> requires no additional settings.</p>
+<p><strong>Instructions:</strong><br />
+Include the Hide It short tag in your post or page content.
+</p>
+<strong>Example:</strong><br />
+<code>[hideit hide=".widget-1, #header-div" show=".widget-2"]</code> 
+<p>A .dot indicates this is a class name, a #hash mark indicates this is an id attribute, and a comma separates each object. Easy enough. You can get an object's id or class name by viewing the source code for your webpage.</p>
+<br />
+<hr />
+<a href="http://www.gnu.org/licenses/gpl.html" target="_blank"><img src="http://www.gnu.org/graphics/gplv3-127x51.png" alt="GNU/GPL" border="0" /></a><em><strong>Share And Share-Alike!</strong></em><br />
+<code><strong>Another <em><strong>Quality</strong></em> Work From  <a href="http://JasonLau.biz" target="_blank">JasonLau.biz</a></strong> - &copy;2011 Jason Lau</code>
+</div>
+<?php
+}
+
+
+function hideit_hook($atts, $content) {
+    $hide_objects = explode(",", $atts['hide']);
+    $show_objects = explode(",", $atts['show']);
+    $output = "\n\n<!-- HIDE IT -->\n"
+             ."<script type=\"text/javascript\">\n"
+             ."jQuery(function(){\n";
+    if(count($hide_objects) > 0)
+    foreach($hide_objects as $hide):
+    $output .= "jQuery(\"" . trim($hide) . "\").hide();\n";
+    endforeach;
+    foreach($show_objects as $show):
+    $output .= "jQuery(\"" . trim($show) . "\").show();\n";
+    endforeach;                
+    $output .= "});\n"
+              ."</script>\n"
+              ."<!-- HIDE IT -->\n\n";
+    return $content.$output;    
+}
+
+function hideit_admin_menu(){
+    add_submenu_page('tools.php', 'Hide It', 'Hide It', 'update_plugins', 'hide-it', 'hideit_manager');
+}
+
+function hideit_install(){
+   add_option('_data', '');
+}
+
+function hideit_deactivate(){
+    delete_option('_data');
+}
+
+function hideit_init() {
+    wp_enqueue_script('jquery');            
+} 
+   
+add_shortcode('hideit', 'hideit_hook');
+add_action('init', 'hideit_init');
+add_action('admin_menu', 'hideit_admin_menu');
+register_activation_hook(__FILE__, 'hideit_install');
+register_deactivation_hook(__FILE__, 'hideit_deactivate');
+?>
